6dbd5983e41137fd4123a31ac074da7d1e04e811
JustZoB <37073994+JustZoB@users.noreply.github.com>
2020-10-22 23:55:23 +0600

    Add hovers for inputs and buttons, add burger menu, fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+let inputs = document.querySelectorAll(".form-input");
+
+inputs.forEach(function(el, index) {
+    el.addEventListener('focus', () => {
+        el.parentElement.classList.add("focused");
+    });
+    el.addEventListener('blur', () => {
+        if (el.value == "") {
+            el.classList.remove("filled");
+            el.parentElement.classList.remove("focused");
+        } else {
+            el.classList.add("filled");
+        }
+    });
+});
